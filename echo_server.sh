37f278365029d5352cf92addb4b729ab10703376
Ariel Angeles <52549146+arielangeles@users.noreply.github.com>
2021-02-11 19:16:38 -0400

    Update echo_server.sh

--- echo_server.sh
+++ echo_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # set port to be used
-port = 5432
+port=5432
 
 # echo a message ($1 acts as first parameter)
 echo $1
 
-# open socket connection
+# open socket connection (socat must be installed)
 socat TCP4-LISTEN:${port},fork EXEC:cat
