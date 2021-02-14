27c6802e187b2eef587571100cfeb4ef41d65f04
Ariel Angeles <52549146+arielangeles@users.noreply.github.com>
2021-02-13 23:55:36 -0400

    Update echo_server.sh

--- echo_server.sh
+++ echo_server.sh
@@ -3,8 +3,5 @@
 # set port to be used
 port=5432
 
-# echo a message ($1 acts as first parameter)
-echo $1
-
-# open socket connection (socat must be installed)
-socat TCP4-LISTEN:${port},fork EXEC:cat
+# echo a message ($1 acts as first parameter) | send message through localhost and specified port
+echo $1 | nc localhost $port
